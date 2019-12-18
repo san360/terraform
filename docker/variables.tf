3c0489f2740f7ec7efe37f099c3e16ca96506a5b
Singh, Sanjay-1 (Ext) <sanjay-1.singh@novartis.com>
2019-12-18 10:06:46 +0100

    added tfvars

--- docker/variables.tf
+++ docker/variables.tf
@@ -6,18 +6,10 @@ variable "env" {
 variable "container_name" {
   description = "Name of container"
   type = "map"
-  default = {
-    dev = "dev_blog"
-    prod = "prod_blog"
-  }
 }
 variable "image" {
   description = "image for container"
   type = "map"
-  default = {
-    dev ="ghost:latest"
-    prod ="ghost:alpine"
-  }
 }
 variable "int_port" {
   description = "internal port for container"
