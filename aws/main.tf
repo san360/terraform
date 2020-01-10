6afb7dd5d463f6bd235be57a7ce22dbbc890a9e0
Singh, Sanjay-1 (Ext) <sanjay-1.singh@novartis.com>
2020-01-10 06:34:31 +0100

    terraform fmt

--- aws/main.tf
+++ aws/main.tf
@@ -1,12 +1,12 @@
 # Setup provider
 provider "aws" {
-  region = "${var.aws_region}"
+  region                  = "${var.aws_region}"
   shared_credentials_file = "${var.shared_credential_file}"
 }
 
 #Deploy storage resources
 module "s3_bucket" {
-  source = "./storage"
+  source       = "./storage"
   project_name = "${var.project_name}"
 }
 
@@ -16,20 +16,20 @@ output "bucket_name" {
 
 #---Network module-----
 module "network" {
-  source = "./networking"
-  vpc_id = "${var.vpc_id}"
-  security_group_name ="${var.security_group_name}"
-  route_table_id = "${var.route_table_id}"
+  source              = "./networking"
+  vpc_id              = "${var.vpc_id}"
+  security_group_name = "${var.security_group_name}"
+  route_table_id      = "${var.route_table_id}"
 }
 
 #---Compute module---
 module "compute" {
-  source = "./compute"
-  ami_owner = "${var.ami_owner}"
-  key_name="${var.key_name}"
-  key_path="${var.key_path}"
-  instance_type = "${var.instance_type}"
-  instance_count = "${var.instance_count}"
-  subnets = "${module.network.subnets}"
-  security_group_id  = "${module.network.securitygroup}"
+  source            = "./compute"
+  ami_owner         = "${var.ami_owner}"
+  key_name          = "${var.key_name}"
+  key_path          = "${var.key_path}"
+  instance_type     = "${var.instance_type}"
+  instance_count    = "${var.instance_count}"
+  subnets           = "${module.network.subnets}"
+  security_group_id = "${module.network.securitygroup}"
 }
